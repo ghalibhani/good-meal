b22316b970e0694401dd86b69960e9eb5c155290
ghalib pratama <ghalibpratama.gpro@gmail.com>
2024-11-19 10:52:45 +0700

    feat : add navbar

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Navigation() {
+  return (
+    <div className='navbar px-10 shadow bg-base-100'>
+      <div className='flex-1'>
+        <a className='btn btn-ghost text-xl'>Good Meal</a>
+      </div>
+      <div className='flex-none gap-2'>
+        <div className='form-control'>
+          <input
+            type='text'
+            placeholder='Search'
+            className='input input-bordered w-24 md:w-auto'
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navigation;
